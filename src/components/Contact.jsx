154af6409c4ea3ee5f707d9ea5ba11cac335776f
Jackson McGillivary <jackmcg777@protonmail.com>
2023-09-21 16:35:41 -0400

    Add production code

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,19 @@
+import { motion } from "framer-motion";
+
 function Contact() {
   return (
     <div
       id="Contact"
       className="h-screen w-full flex justify-center items-center"
     >
-      <div className="w-4/12 h-screen flex flex-col justify-center items-center">
-        <form className="flex flex-col gap-3 justify-center items-center w-10/12 h-screen">
+      <div className="w-6/12 h-screen flex flex-col justify-center items-center">
+        <motion.div
+          whileHover={{ scale: 1.2 }}
+          whileTap={{ scale: 1.1 }}
+          drag="x"
+          dragConstraints={{ left: -100, right: 100 }}
+        />
+        <form className="flex flex-col gap-3 justify-center items-center rounded-xl w-8/12 h-12/12 p-8 hover:shadow-lg ease-out duration-700">
           <h2 className="text-6xl font-secondary border-accent1 border-b-2 mb-8">
             Reach me
           </h2>
@@ -14,9 +22,7 @@ function Contact() {
               className="flex flex-col w-full justify-center items-center mr-2"
               htmlFor="fname"
             >
-              <span className="text-xl font-secondary">
-                Your full name{" "}
-              </span>
+              <span className="text-xl font-secondary">Your full name </span>
               <input
                 className="border border-secondary rounded-xl p-4 m-2 w-full"
                 type="text"
@@ -54,10 +60,10 @@ function Contact() {
             />
           </label>
           <button
-            className="border border-secondary rounded-full h-12 w-4/12 mt-8"
+            className="border border-secondary rounded-full h-12 w-4/12 mt-4"
             type="button"
           >
-            Shoot
+            <span className=" border-b-2 border-accent1">Shoot</span>
           </button>
         </form>
       </div>
