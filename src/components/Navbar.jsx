7c64f7399cabe4b3aa7515d1425ccfac73dee282
Jackson McGillivary <jackmcg777@protonmail.com>
2023-09-24 23:15:22 -0400

    Add navbar border switching

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,52 @@
-import { NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom";
 
-function Navbar(){
-    return(
-        <div className="App fixed">
-            <nav className="flex flex-row gap-8 p-6 pl-8">
-                <NavLink className='font-serif text-2xl font-medium' to='/'>
-                    Home
-                </NavLink>
-                <NavLink className='font-serif text-2xl font-medium' to='/Projects'>
-                    Projects
-                </NavLink>
-                <NavLink className='font-serif text-2xl font-medium'to='/About'>
-                    About
-                </NavLink>
-                <NavLink className='font-serif text-2xl font-medium' to='/Contact'>
-                    Contact
-                </NavLink> 
-            </nav>
-        </div>
-    )
-
+function Navbar() {
+  return (
+    <div className="App fixed">
+      <nav className="flex flex-row gap-8 p-6 pl-8">
+        <NavLink
+          className={({ isActive }) =>
+            isActive
+              ? "font-serif text-2xl font-medium border-b-2 border-accent1"
+              : "font-serif text-2xl font-medium"
+          }
+          to="/"
+        >
+          Home
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            isActive
+              ? "font-serif text-2xl font-medium border-b-2 border-accent1"
+              : "font-serif text-2xl font-medium"
+          }
+          to="/Projects"
+        >
+          Projects
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            isActive
+              ? "font-serif text-2xl font-medium border-b-2 border-accent1"
+              : "font-serif text-2xl font-medium"
+          }
+          to="/About"
+        >
+          About
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            isActive
+              ? "font-serif text-2xl font-medium border-b-2 border-accent1"
+              : "font-serif text-2xl font-medium"
+          }
+          to="/Contact"
+        >
+          Contact
+        </NavLink>
+      </nav>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
