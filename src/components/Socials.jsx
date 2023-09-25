8e6f6c1bf30ff57dacbbee3ff239fc2414e39df0
Jackson McGillivary <jackmcg777@protonmail.com>
2023-09-25 01:29:28 -0400

    Change add scale animation to social links

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -1,18 +1,54 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faTwitter, faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTwitter,
+  faLinkedin,
+  faGithub,
+} from "@fortawesome/free-brands-svg-icons";
+import { motion } from "framer-motion";
+
 
 function Socials() {
-    return (
-        <>
-            <div className="fixed left-0 top-0 h-screen">
-                <div className="h-screen flex flex-col justify-center items-start gap-16 pl-8 bg-transparent">
-                    <a href="https://twitter.com/Revenant_Dev" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faTwitter} size={"lg"}/></a>
-                    <a href="https://www.linkedin.com/in/jackson-mcgillivary-297802266/" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faLinkedin} size={"lg"}/></a>
-                    <a href="https://github.com/Revenant70" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithub} size={"lg"}/></a>
-                </div>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="fixed left-0 top-0 h-screen">
+        <div className="h-screen flex flex-col justify-center items-start gap-16 pl-8 bg-transparent">
+          <motion.a
+            href="https://twitter.com/Revenant_Dev"
+            target="_blank"
+            rel="noreferrer"
+
+            whileHover={{
+                scale: 1.2
+            }}
+          >
+            <FontAwesomeIcon id="faTwitter" icon={faTwitter} size={"lg"}/>
+          </motion.a>
+          <motion.a
+            href="https://www.linkedin.com/in/jackson-mcgillivary-297802266/"
+            target="_blank"
+            rel="noreferrer"
+
+            whileHover={{
+                scale: 1.2
+            }}
+          >
+            <FontAwesomeIcon icon={faLinkedin} size={"lg"}/>
+          </motion.a>
+          <motion.a
+            href="https://github.com/Revenant70"
+            target="_blank"
+            rel="noreferrer"
+
+            whileHover={{
+                scale: 1.2
+            }}
+          >
+            <FontAwesomeIcon icon={faGithub} size={"lg"} />
+          </motion.a>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Socials
+export default Socials;
