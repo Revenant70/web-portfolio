20f994ab6dd3fa6b3f93396a1cb35aad062301af
Jackson McGillivary <jackmcg777@protonmail.com>
2023-09-29 15:43:14 -0400

    Add: Dark theme to quote card

--- src/components/QuoteCard.jsx
+++ src/components/QuoteCard.jsx
@@ -5,7 +5,7 @@ export default function QuoteCard(props) {
     <AnimatePresence>
         {props.trigger && (
             <motion.div
-            className="bg-primary shadow-xl flex flex-row w-72 h-48 rounded-2xl absolute top-[-0px] left-72"
+            className="bg-primary dark:bg-darkPrimary shadow-xl flex flex-row w-72 h-48 rounded-2xl absolute top-[-0px] left-72"
             initial={{
               x: -100,
               opacity: 0,
@@ -28,20 +28,20 @@ export default function QuoteCard(props) {
             <motion.div className="flex flex-col justify-center items-start">
               {props.children}
               <div className="flex flex-col pl-4">
-                <h3 className="text-3xl font-secondary">en·​thrall·ed</h3>
-                <p className="text-xl font-serif">
-                  variants or <span className="text-accent1">enthral</span>
+                <h3 className="text-3xl font-secondary text-secondary  dark:text-darkSecondary">en·​thrall·ed</h3>
+                <p className="text-xl font-serif text-secondary  dark:text-darkSecondary">
+                  variants or <span className="text-accent1 dark:text-darkAccent1">enthral</span>
                 </p>
-                <p className="text-lg font-serif text-accent2 opacity-70">
+                <p className="text-lg font-serif text-accent2 opacity-70 dark:opacity-100 dark:text-darkSecondary">
                   enthral; enthralling
                 </p>
               </div>
-              <div className="border-b-2 border-accent1 pt-1 w-40 ml-4"></div>
+              <div className="border-b-2 border-accent1 dark:border-darkAccent1 pt-1 w-40 ml-4"></div>
               <div className="pl-4 pt-1">
-                <p className="font-serif text-xl opacity-90">
+                <p className="font-serif text-xl opacity-90 dark:opacity-100 text-secondary  dark:text-darkSecondary">
                   1 - to hold spellbound : charm
                 </p>
-                <p className="font-serif text-xl opacity-90">
+                <p className="font-serif text-xl opacity-90 dark:opacity-100 text-secondary  dark:text-darkSecondary">
                   2 - to hold in or reduce to slavery
                 </p>
               </div>
