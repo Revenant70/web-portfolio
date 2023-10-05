62b6bff4c33b97dbed28e15ef9f22448cbbb3f78
Revenant70 <jackmcg777@protonmail.com>
2023-10-05 16:03:42 -0400

    Add: Contact form sends data to email

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,36 @@
 import { motion } from "framer-motion";
+import emailjs from "@emailjs/browser";
+import { useRef } from "react";
 
 function Contact() {
+  const form = useRef();
+  const FullName = useRef();
+  const UserEmail = useRef();
+  const UserSubject = useRef();
+  const UserMessage = useRef();
+
+  // Email verification
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_j08g1md",
+        "template_mabmnu9",
+        form.current,
+        "-ZaBDSJlWpaS7mqWx"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          form.current.reset();
+        },
+        (error) => {
+          console.log(error.text);
+        },
+      );
+  };
+
   return (
     <div
       id="Contact"
@@ -13,7 +43,12 @@ function Contact() {
           drag="x"
           dragConstraints={{ left: -100, right: 100 }}
         />
-        <form className="flex flex-col gap-3 justify-center items-center rounded-xl w-full sm:w-9/12 md:w-full lg:w-10/12 xl:w-9/12 2xl:w-5/12 h-12/12 p-8">
+        <form
+          className="flex flex-col gap-3 justify-center items-center rounded-xl w-full sm:w-9/12 md:w-full lg:w-10/12 xl:w-9/12 2xl:w-5/12 h-12/12 p-8"
+          id='form'
+          onSubmit={sendEmail}
+          ref={form}
+        >
           <h2 className="text-6xl text-center font-secondary border-accent1 dark:border-darkAccent1 text-secondary dark:text-darkSecondary border-b-2 pb-4">
             Reach me
           </h2>
@@ -31,13 +66,13 @@ function Contact() {
                 id="fname"
                 name="user_name"
                 placeholder="John Doe"
+                ref={FullName}
                 required
               />
             </label>
             <label
               className="flex flex-col w-full justify-center items-center ml-2"
               htmlFor="email"
-              required
             >
               <span className="text-xl font-secondary text-secondary dark:text-darkSecondary">
                 Your email*{" "}
@@ -48,19 +83,25 @@ function Contact() {
                 id="email"
                 name="user_email"
                 placeholder="johndoe@gmail.com"
+                ref={UserEmail}
+                required
               />
             </label>
           </div>
-          <label htmlFor="subject" className="flex flex-col w-full justify-center items-center">
+          <label
+            htmlFor="subject"
+            className="flex flex-col w-full justify-center items-center"
+          >
             <span className="text-xl font-secondary mb-3 text-secondary dark:text-darkSecondary">
               Subject{" "}
             </span>
             <input
               className="border dark:border-none bg-primary dark:bg-darkSecondary dark:placeholder-darkAccent2 text-secondary dark:text-darkAccent2 rounded-xl p-4 m-2 w-full"
               type="text"
-              id="email"
+              id="subject"
               name="user_subject"
               placeholder="Enter your subject"
+              ref={UserSubject}
             />
           </label>
           <label
@@ -77,11 +118,12 @@ function Contact() {
               name="user_message"
               placeholder="Hey Jackson, I wanted to discuss collaborating"
               required
+              ref={UserMessage}
             />
           </label>
           <button
             className="border border-secondary dark:bg-darkAccent1 dark:border-none rounded-full h-12 w-4/12 mt-4"
-            type="button"
+            type="submit"
           >
             <span className=" border-b-2 border-accent1 dark:border-none">
               Shoot
