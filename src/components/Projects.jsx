aa8dfc17234ab09b0168bccbff231dc3cb3dbf38
Revenant70 <jackmcg777@protonmail.com>
2023-10-09 17:36:53 -0400

    Add: Snap page scrolling for projects page

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,23 +1,24 @@
 import { ProjectInfo } from "./ProjectInfo";
 import { ProjectBox } from "./ProjectBox";
 
+
 function Projects() {
   return (
-    <div className="h-screen w-screen">
+    <div className="h-screen w-screen snap-y snap-mandatory overflow-y-auto">
       <div className="bg-primary dark:bg-darkPrimary flex flex-col justify-center items-center px-8">
-        <section className="flex flex-col md:flex-row justify-center items-center h-screen w-11/12 md:w-10/12">
+        <section className="flex flex-col md:flex-row justify-center items-center h-screen w-11/12 md:w-10/12 snap-start">
           <ProjectInfo></ProjectInfo>
           <ProjectBox></ProjectBox>
         </section>
-        <section className="flex flex-col-reverse md:flex-row justify-center items-center h-screen w-11/12 md:w-10/12">
+        <section className="flex flex-col-reverse md:flex-row justify-center items-center h-screen w-11/12 md:w-10/12 snap-start">
           <ProjectBox></ProjectBox>
           <ProjectInfo right={"md:items-end"}></ProjectInfo>
         </section>
-        <section className="flex flex-col md:flex-row justify-center items-center h-screen w-11/12 md:w-10/12">
+        <section className="flex flex-col md:flex-row justify-center items-center h-screen w-11/12 md:w-10/12 snap-start">
           <ProjectInfo></ProjectInfo>
           <ProjectBox></ProjectBox>
         </section>
-        <section className="flex flex-col-reverse md:flex-row justify-center items-center h-screen w-11/12 md:w-10/12">
+        <section className="flex flex-col-reverse md:flex-row justify-center items-center h-screen w-11/12 md:w-10/12 snap-start">
           <ProjectBox></ProjectBox>
           <ProjectInfo right={"md:items-end"}></ProjectInfo>
         </section>
